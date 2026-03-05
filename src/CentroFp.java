3a5593e8a82a5e8c0995ca4d540308d21666abd8
alexe <aerneher@myuax.com>
2026-03-05 09:25:08 +0100

    feat(centroFP): Inilizacion de la array y creacion del primer metodo publico

---
+++ src/CentroFp.java
@@ -0,0 +1,33 @@
+public class CentroFp {
+
+    private Alumno[] alumnos;
+    private int MAX_ALUMNOS;
+
+    public CentroFp(int cantidad_maxima_alumnos) {
+       MAX_ALUMNOS = cantidad_maxima_alumnos;
+       alumnos = new Alumno[MAX_ALUMNOS];
+    }
+
+    public Alumno buscarAlumno(int id){
+
+
+    }
+
+
+    private int buscarPrimerHuecoLibre(){
+
+    }
+
+
+
+    public boolean registrarAlumno(Alumno alumno){}
+
+
+
+    public String mostrarAlumnos(){
+    }
+
+
+
+    public int contarAlumnos(){}
+}
